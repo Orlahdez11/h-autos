8c07de47c12322deb6640b127a342c4c14d69b53
Orlahdez11 <orlahdez2006@gmail.com>
2026-04-17 09:58:18 -0600

    Actualicé mi proyecto

--- conexiones/db.js
+++ conexiones/db.js
@@ -3,7 +3,12 @@ require("dotenv").config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: 
+    process.env.DATABASE_URL && 
+    !process.env.DATABASE_URL.includes("localhost") && 
+    !process.env.DATABASE_URL.includes("127.0.0.1")
+      ? { rejectUnauthorized: false }
+      : false
 });
 
 module.exports = pool;
